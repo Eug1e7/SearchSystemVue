7ec15b9138abfbf25e01392a998abda287e7f5f7
Euglena <shimidorimu.00@gmail.com>
2024-03-04 00:35:32 +0900

    コードの変更を説明するための短く簡潔なコミットメッセージです。***
    ***削除されたファイルと関連するコードの変更が含まれています。***
    
    ***変更内容: ファイルの削除***
    
    ***変更内容: Viteの設定ファイルの削除***
    
    ***変更内容: コンポーネントの型定義ファイルの削除***
    
    ***変更内容: メインのJavaScriptファイルの削除***
    
    ***変更内容: グローバルな型定義ファイルの削除***
    
    ***変更内容: Viteの設定ファイルのタイムスタンプファイルの削除***
    
    ***以上がコードの変更内容です。

--- src/components/SearchHistoryComponent.vue.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: import("vue").DefineComponent<{}, {}, {
-    searchHistory: never[];
-    loading: boolean;
-    error: null;
-}, {}, {
-    fetchSearchHistory(): Promise<void>;
-}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<import("vue").ExtractPropTypes<{}>>, {}, {}>;
-export default _default;
--- src/components/SearchResultComponent.vue.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: import("vue").DefineComponent<{}, {}, {
-    loading: boolean;
-    error: null;
-    results: string;
-}, {}, {
-    fetchResults(): Promise<void>;
-    adjustHeight(): void;
-}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<import("vue").ExtractPropTypes<{}>>, {}, {}>;
-export default _default;
